9519aafe8b8d89a468a018086787739c8bfd665d
Hoa Nguyen <hoanguyen@ucdavis.edu>
2022-11-01 12:09:50 -0700

    Move common scripts up 1 directory
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>

--- project-1-max-out-bandwith/experiment-1-STREAM-gem5-experiment/run.sh
+++ project-1-max-out-bandwith/experiment-1-STREAM-gem5-experiment/run.sh
@@ -1,2 +1,2 @@
-python3 generate_riscv_n_elements.py riscv_n_elements.txt
-python3 compile_STREAM.py
+python3 ../utils/generate_riscv_n_elements.py riscv_n_elements.txt
+python3 ../utils/compile_STREAM.py
