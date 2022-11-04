6ee113b2f1eb96e2cea7228e903879bcef780175
Hoa Nguyen <hoanguyen@ucdavis.edu>
2022-11-04 16:45:00 -0700

    Update compile_STREAM script to support optional m5 annotations
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>

--- project-1-max-out-bandwith/utils/compile_STREAM.py
+++ project-1-max-out-bandwith/utils/compile_STREAM.py
@@ -11,13 +11,16 @@ from pathlib import Path
 """
 
 class Config:
-    def __init__(self, workspace_path, n_elements_path, stream_repo, isa, n_threads):
+    def __init__(self, workspace_path, n_elements_path, stream_repo, isa, n_threads, with_m5_annotations, m5_build_abspath, m5ops_header_abspath):
         self.workspace_path = workspace_path
         self.n_elements_path = n_elements_path
         self.stream_repo = stream_repo
         self.isa = isa
         assert(isa in {"riscv", "arm", "x86"})
         self.n_threads = n_threads
+        self.with_m5_annotations = with_m5_annotations
+        self.m5_build_abspath = m5_build_abspath
+        self.m5ops_header_abspath = m5ops_header_abspath
 
 def warn(message):
     print(f"warn: {message}")
@@ -43,11 +46,45 @@ def download_stream(configs):
     assert(error is None)
     return output
 
-def compile_stream_helper_riscv(n_elements, n_threads, stream_repo_path, output_path):
+def compile_stream_helper_riscv(n_elements, n_threads, stream_repo_path, output_path, with_m5_annotations, m5_build_abspath, m5ops_header_abspath):
     stream_repo_abspath = os.path.abspath(stream_repo_path)
+
+    # some hardcoded vals
+    guest_m5_build_path = "/m5_build"
+    guest_m5ops_header_path = "/m5ops_header"
+    guest_STREAM_path = "/STREAM"
+    guest_compiler_path = "/riscv/_install/bin/riscv64-unknown-linux-gnu-gcc"
+    docker_image = "hn/mpi"
+    workspace_path = guest_STREAM_path
+
+    # getting the right uid and gid
     uid = os.getuid()
     gid = os.getgid()
-    command = f"docker run --rm --volume {stream_repo_abspath}:/STREAM -w /STREAM -u {uid}:{gid} -e CC=/riscv/_install/bin/riscv64-unknown-linux-gnu-gcc -e ARRAY_SIZE={n_elements} -e N_THREADS={n_threads} hn/mpi make"
+
+    # linking directory from host to guest
+    directory_links = [f"{stream_repo_abspath}:{guest_STREAM_path}"] 
+    if with_m5_annotations:
+        directory_links.append(f"{m5_build_abspath}:{guest_m5_build_path}")
+        directory_links.append(f"{m5ops_header_abspath}:{guest_m5ops_header_path}")
+    # convert the links to a string
+    directory_link_str = ""
+    for link in directory_links:
+        directory_link_str += f"--volume {link} "
+
+    # setting up the environment variables
+    env = {}
+    env["CC"] = guest_compiler_path
+    env["ARRAY_SIZE"] = n_elements
+    env["N_THREADS"] = n_threads
+    if with_m5_annotations:
+        env["M5_BUILD_PATH"] = guest_m5_build_path
+        env["M5OPS_HEADER_PATH"] = guest_m5ops_header_path
+    # constructing the string specifying the environment variables
+    env_str = ""
+    for key, val in env.items():
+        env_str += f"-e {key}={val} "
+
+    command = f"docker run --rm {directory_link_str} -w {workspace_path} -u {uid}:{gid} {env_str} {docker_image} make"
     output, error = run_cmd(command)
     assert(error is None)
     
@@ -74,14 +111,23 @@ def compile_stream(configs):
         line = line.strip().split()
         n_elements_all = list(map(int, line))
     for n_elements in n_elements_all:
-        compiling_helper(n_elements, configs.n_threads, str(stream_repo_path), str(output_path))
+        compiling_helper(n_elements=n_elements,
+                         n_threads=configs.n_threads,
+                         stream_repo_path=str(stream_repo_path),
+                         output_path=str(output_path),
+                         with_m5_annotations=configs.with_m5_annotations,
+                         m5_build_abspath=configs.m5_build_abspath,
+                         m5ops_header_abspath=configs.m5ops_header_abspath)
 
 if __name__ == "__main__":
     configs = Config(workspace_path = "build",
                     n_elements_path = os.path.abspath("riscv_n_elements.txt"),
                     stream_repo = "https://github.com/takekoputa/STREAM",
                     isa = "riscv",
-                    n_threads=4
+                    n_threads=4,
+                    with_m5_annotations=True,
+                    m5_build_abspath=os.path.abspath("gem5/util/m5/build/riscv/"),
+                    m5ops_header_abspath=os.path.abspath("gem5/include/")
                     )
     workspace_path = Path(configs.workspace_path)
     workspace_path.mkdir(exist_ok=True)
