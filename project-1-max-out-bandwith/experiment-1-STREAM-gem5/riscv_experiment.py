cc48a450e226acd3623afee40c3c58433a4dac30
Hoa Nguyen <hoanguyen@ucdavis.edu>
2022-11-12 03:21:52 +0000

    Update the runscript to incorporate num_elements as metadata
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>

--- project-1-max-out-bandwith/experiment-1-STREAM-gem5/riscv_experiment.py
+++ project-1-max-out-bandwith/experiment-1-STREAM-gem5/riscv_experiment.py
@@ -7,9 +7,9 @@ import subprocess
 import multiprocessing
 
 experiment_tag = "riscv-stream-1"
-gem5_binary_path = "/home/hn/experiments/STREAM-gem5-experiment/gem5-dev/build/RISCV/gem5.opt"
-gem5_config_path = "/home/hn/experiments/STREAM-gem5-experiment/configs/gem5/riscvmatched-stream.py"
-gem5_output_path_prefix = "/home/hn/experiments/STREAM-gem5-experiment/results/"
+gem5_binary_path = "/scr/hn/gem5-takekoputa-stream/build/RISCV/gem5.opt"
+gem5_config_path = "/home/hn/experiments/project-1-max-out-bandwith/experiment-1-STREAM-gem5/configs/gem5/riscvmatched-stream.py"
+gem5_output_path_prefix = "/home/hn/experiments/project-1-max-out-bandwith/experiment-1-STREAM-gem5/results"
 
 def get_md5sum(filepath):
     filepath = str(filepath)
@@ -22,15 +22,15 @@ def get_md5sum(filepath):
         return None
     return md5sum.decode("utf-8")
 
-# the gem5 binary in /scr/hn/gem5-dev-x86/build/RISCV/gem5.opt
-gem5_binary_md5sum = "274bfa021cace308be8bebbe29c3828b"
+# the gem5 binary in /scr/hn/gem5-takekoputa-stream/build/RISCV/gem5.opt in azacca
+gem5_binary_md5sum = "2c99f9bd5a4437fb048eb29d79e04bcb"
 
 def sanity_check():
     assert(Path(gem5_binary_path).exists())
     assert(Path(gem5_output_path_prefix).exists())
 
-def output_folder_generator(isa, stream_binary_path, num_cores):
-    return ";".join([isa, Path(stream_binary_path).name, num_cores])
+def output_folder_generator(isa, num_elements, num_cores):
+    return "-".join([isa, num_elements, num_cores])
 
 def gem5_params_generator(output_path, stream_binary_path, num_cores):
     gem5_params = {}
@@ -50,26 +50,31 @@ def gem5_params_generator(output_path, stream_binary_path, num_cores):
 
     return gem5_params, config_params
 
-def metadata_generator(stream_binary_md5sum):
+def metadata_generator(stream_binary_md5sum, stream_binary_path, num_elements, num_cores):
     metadata = {}
 
     metadata["tag"] = experiment_tag
-    metadata["git-hash"] = "d99f9389fef6a10f79da82a4edb5c3e296c6fc77"
+    metadata["git-hash"] = "553096ee5333d646f8a6a0e1ec070f64f1fca498(develop) + c34e1d0c30bb8f7ab839856e815f548d6e422d80(mine)"
     metadata["git-remote"] = "https://github.com/takekoputa/gem5"
-    metadata["git-branch"] = "stream-experiment"
+    metadata["git-branch"] = "stream-experiments"
 
     metadata["gem5-binary-md5sum"] = gem5_binary_md5sum
     metadata["stream-binary-md5sum"] = stream_binary_md5sum
 
+    metadata["stream-binary-path"] = stream_binary_path
+    metadata["num-elements"] = num_elements
+    metadata["num-cores"] = num_cores
+
     return metadata
 
-def generate_stream_experiment_unit(stream_binary_path, num_cores):
+def generate_stream_experiment_unit(experiment_tag, stream_binary_path, num_elements, num_cores):
     num_cores = str(num_cores)
+    num_elements = str(num_elements)
     assert(isinstance(stream_binary_path, str))
     assert(isinstance(num_cores, str))
 
-    output_folder_name = output_folder_generator("riscv", stream_binary_path, num_cores)
-    output_path = str(Path(gem5_output_path_prefix) / output_folder_name)
+    output_folder_name = output_folder_generator("riscv", num_elements, num_cores)
+    output_path = str(Path(gem5_output_path_prefix) / experiment_tag / output_folder_name)
 
     gem5_params, config_params = gem5_params_generator(output_path, stream_binary_path, num_cores)
 
@@ -80,7 +85,7 @@ def generate_stream_experiment_unit(stream_binary_path, num_cores):
                           config_params = config_params,
                           env = env)
 
-    metadata = metadata_generator(get_md5sum(stream_binary_path))
+    metadata = metadata_generator(get_md5sum(stream_binary_path), stream_binary_path, num_elements, num_cores)
     for key, val in metadata.items():
         unit.add_metadata(key, val)
 
@@ -94,9 +99,19 @@ if __name__ == "__main__":
 
     experiment = Experiment()
 
-    for stream_binary_path in Path("build/binaries/riscv/").glob("stream_c.*"):
-        stream_binary_path = str(stream_binary_path)
-        unit = generate_stream_experiment_unit(stream_binary_path=stream_binary_path, num_cores=5)
+    #for stream_binary_path in Path("build/binaries/riscv/").glob("stream_c.*"):
+    #    stream_binary_path = str(stream_binary_path)
+    #    unit = generate_stream_experiment_unit(experiment_tag, stream_binary_path=stream_binary_path, num_cores=5)
+    #    experiment.add_experiment_unit(unit)
+
+    num_elements_list = []
+    with open("riscv_n_elements.txt", "r") as f:
+        line = f.readline().strip().split()
+        num_elements_list = list(map(int, line))
+
+    for num_elements in num_elements_list:
+        stream_binary_path = str(Path(".") / "build" / "binaries" / "riscv" / f"stream_c.{num_elements}")
+        unit = generate_stream_experiment_unit(experiment_tag=experiment_tag, stream_binary_path=stream_binary_path, num_elements=num_elements, num_cores=5)
         experiment.add_experiment_unit(unit)
 
     n_processes = multiprocessing.cpu_count() // 2
--- project-1-max-out-bandwith/experiment-1-STREAM-gem5/run.sh
+++ project-1-max-out-bandwith/experiment-1-STREAM-gem5/run.sh
@@ -1,2 +1,2 @@
 python3 ../utils/generate_riscv_n_elements.py riscv_n_elements.txt
-python3 ../utils/compile_STREAM.py
+python3 ../utils/compile_STREAM.py configs/STREAM_compilation/config.json -j10
